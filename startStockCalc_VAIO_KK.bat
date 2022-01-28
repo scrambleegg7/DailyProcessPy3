f11d31e2d345ecd2b21a021b4ca484ed324533d0
TKVAIOZBLK\miyuk_000 <scrambleegg7@gmail.com>
2022-01-28 10:29:42 +0900

    Special batch script for KikyuchiKaya userID (microsoft id : miyukiph_kk) on VAIO

---
+++ startStockCalc_VAIO_KK.bat
@@ -0,0 +1,28 @@
+echo "-------"
+rem L:
+rem cd /D \\ipython\\DailyprocessPy3
+
+chcp 932
+
+set LOGPATH=l:\\ipython\\DailyprocessPy3\\logs\\batch.log
+
+echo $PATH 
+
+set T=%TIME:~0,8%
+set T=%T: =0%
+ 
+echo %DATE% %T% %1 %~2
+echo %DATE% %T% %1 %~2 > %LOGPATH% 2>&1
+
+
+c:\Users\miyuk_001\anaconda3\python.exe l:\\ipython\\DailyprocessPy3\\stockCalc.py >> %LOGPATH% 2>&1
+
+c:\Users\miyuk_001\anaconda3\python.exe l:\\ipython\\DailyprocessPy3\\DeadStock.py >> %LOGPATH% 2>&1
+
+
+REM pause
+
+REM ====== StockCalculation data loader
+
+
+
